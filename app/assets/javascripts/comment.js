3d075a6aeb195b4181d900586e34a6d0d3af8099
matsumotoyukari <yukari@MATSUMOTOnoMacBook-ea.local>
2018-12-07 21:32:21 +0900

    画像送信機能を追加しました

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,17 +1,18 @@
 $(document).on('turbolinks:load', function() {
-  function buildHTML(comment){
-    // comment = data
-    var html = `<div class="message" id=${ comment.user_id }>
+  function buildHTML(message){
+    var MessageImage = (message.image) ? `<img src="${message.image}">` : ``
+    var html = `<div class="message">
                 <div class="sent-name">
-                  ${ comment.user_name }
+                ${ message.user_name }
                 </div>
                 <div class="sent-day">
-                  ${ comment.created_at }
+                ${ message.created_at }
                 </div>
-                <div class="sent-naiyo">
-                  ${ comment.content }
-                </div>
-                </div>`
+                  <div class="sent-naiyo">
+                  ${ message.content }
+                  </div>
+                ${ MessageImage }
+                </div>`;
     return html;
   }
 
@@ -19,7 +20,6 @@ $(document).on('turbolinks:load', function() {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
-
     $.ajax({
       url: url,
       type: "POST",
@@ -29,69 +29,19 @@ $(document).on('turbolinks:load', function() {
       contentType: false
     })
     .done(function(data){
-      // data = dataOrange
-      // console.log(data)
       var html = buildHTML(data);
-
-      // console.log(html)
-      $('.chat').append(html)
-      $('.textbox').val('')
+      $('.chat').append(html);
+      $('.textbox').val('');
       var height = $('.test').innerHeight()
-      $('.chat').animate({scrollTop: height}, 500, 'swing');
+      $('.chat').animate({scrollTop: $('.chat')[0].scrollHeight}, 'fast');
+      $('form')[0].reset();
     })
     .fail(function(){
       alert('error');
     })
-    //ここから
+
     .always(function(){
       $(".form__submit").removeAttr("disabled");
     });
   })
 })
-
-
-
-
-// $(function(){
-//     $('.form__submit').click(function(){
-//         $('body').animate({
-//           scrollTop: $(document).height()
-//         },1500);
-//         return false;
-//     });
-// });
-
-
-// POST   /groups/:group_id/messages(.:format) messages#create
-
-
- // product_reviews POST   /products/:product_id/reviews(.:format)     reviews#create
- // url: '/products/search',
-
-
-
-
-    // .done(function(){
-    //   // var html = buildHTML(data);
-    //   console.log("成功成功")
-    //   // $('.comments').append(html)
-    //   // $('.textbox').val('')
-    // })
-    // .fail(function(){
-    //   console.log("失敗")
-    // })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,19 +1,6 @@
-#   json.text  @comment.text
-#   json.user_id  @comment.user.id
-#   json.user_name  @comment.user.nickname
-
-
-# json.array! @products do |product|
-#   json.id product.id
-#   json.title product.title
-#   json.image product.image_url
-#   json.detail product.detail
-# end
 json.user_id  @message.user.id
 json.user_name  @message.user.name
-json.created_at  @message.created_at
+json.created_at  @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
 json.content  @message.content
 json.group_id  @message.group_id
-json.image  @message.image
-
-# data{user_id, user_name, cre...} -
+json.image  @message.image.url
